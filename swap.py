090de521ea7c21a0dcf9e9c28d9137f640cd18cc
Rohit1902 <rohitroy1902@gmail.com>
2021-09-29 23:44:59 +0530

    Swappping values of two variables

---
+++ swap.py
@@ -0,0 +1,25 @@
+# PROGRAM TO SWAP AND MANIPULATE INPUTS PROVIDED BY USER
+a=input("Enter first name : ")
+r=input("Enter second name : ")
+
+print("Names before swap :")
+print("a=",a)
+print("\n")
+print("r=",r)
+print("\n")
+
+t=a
+a=r
+r=t
+print("Names after swap :")
+print("a=",a)
+print("\n")
+print("r=",r)
+
+
+a=""
+r=""
+print("Names after manipulation:")
+print("a=",a)
+print("\n")
+print("r=",r)
